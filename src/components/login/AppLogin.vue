ba265f31e90653c9a9a8bb08ead1dbdc459de485
Aleks <aleks444@gmx.de>
2025-01-01 19:45:50 +0100

    connectet to firebase

--- src/components/login/AppLogin.vue
+++ src/components/login/AppLogin.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>Login Page</h1>
+    <input type="text" name="loginName" id="loginName">
+    <input type="password" name="password" id="loginPassword">
+    <button @click="login">Login</button>
     <router-link to="/register">Register</router-link>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { firebaseApp } from './firebase/firebaseConfig';
 
 const app = createApp(App);
 
 app.use(router);
+app.config.globalProperties.$firebaseApp = firebaseApp;
 app.mount('#app');
