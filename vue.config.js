0940a605f3118c07f2d3033789ae76495e9cfc7e
Aleks <aleks444@gmx.de>
2025-01-02 21:58:50 +0100

    bugfix

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@firebase/app': 'firebase/app',
+        '@firebase/firestore': 'firebase/firestore',
+      },
+    },
+  },
+};
